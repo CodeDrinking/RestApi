54cf81d73eabbd77349153f69019fdc1aa84e18d
Roshan Mestri <roshanmestri@SAGAR.bbrouter>
2022-11-18 19:14:46 +0530

    second commit

--- rrrrr/Services/jwtService.js
+++
@@ -1,9 +0,0 @@
-import {JWT_SECRET} from "../config";
-import jwt from 'jsonwebtoken';
-
-class jwtService{
-    static sign(payload , expiry ='60s' , secret=JWT_SECRET){
-        return jwt.sign(payload ,secret , {expiresIn: expiry})
-    }
-}
-export default jwtService;
